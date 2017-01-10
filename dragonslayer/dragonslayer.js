1012b479bdab3eadb3518024ffc9ac032ae95ed3
LarsonDataWonk <entrepreneurial.spirit@outlook.com>
2017-01-10 18:56:47 -0500

    Add files via upload

--- dragonslayer/dragonslayer.js
+++ dragonslayer/dragonslayer.js
@@ -1,75 +1,65 @@
-function dragonslayerFunction(){
+window.onload = function(){ 
 
-alert("Strike the dragon");
+$('#one').append('<button id="button">Play</button>');
+document.getElementById("button").onclick = function() {dragonSlayerFunction()};
 
-var slaying=true;
+function dragonSlayerFunction(){
 
+    //alert("Strike the dragon");
 
-//generate either 0 or 1:
-var youHit = Math.floor(Math.random()*2);
+    var slaying=true;
 
+    //generate either 0 or 1:
+    var youHit = Math.floor(Math.random()*2);
 
-//Generate 0, 1, 2, 3, 4, or 5. (Generate a number <1, multiply it by 5 to get a number <5, add 1 to get a number <6, floor it to get an integer <=5)
+    var damageThisRound;
 
-var damageThisRound = Math.floor(Math.random()*4+1);
+    //var damageThisRound = Math.floor(Math.random()*5+1);
+    var totalDamage=0;
 
+    while(slaying){
 
-//var damageThisRound = Math.floor(Math.random()*5+1);
-
-var totalDamage=0;
-
-
-
-while(slaying){
- 
-   
-if(youHit){
- 
-             
-totalDamage+=damageThisRound;
-   
-var element = document.getElementById("gameStatus");
-var newElement = document.createElement("p");
-element.appendChild(newElement);
-newElement.innerHTML = "Got'm!" + "<br> Damage this round: " + damageThisRound +  "<br> Total damage: " + totalDamage;
- 
-
-if(totalDamage>=4){
- 
- 
-var element = document.getElementById("gameStatus");
-var newElement = document.createElement("p");
-element.appendChild(newElement);
-newElement.innerHTML = "<br> He's dead!";
-  
-slaying=false; 
-  
-
-}else{
-
-alert("Strike the dragon");
+        alert("Strike the dragon");
            
-youHit = Math.floor(Math.random()*2); 
-        
-
-}
-    
-
-}else{
-  
-  
-var element = document.getElementById("gameStatus");
-var newElement = document.createElement("p");
-element.appendChild(newElement);
-newElement.innerHTML = "<br> You missed! He killed you. You lose!";
-        
-
-slaying=false;
+        youHit = Math.floor(Math.random()*2);   
  
-       
-}
+        if(youHit){
+ 
+            //Generate 0, 1, 2, 3, 4, or 5. (Generate a number <1, multiply it by 5 to get a number <5, add 1 to get a number <6, floor it to get an integer <=5)
+            var damageThisRound = Math.floor(Math.random()*4+1);
+            totalDamage+=damageThisRound;
+   
+            var element = document.getElementById("gameStatus");
+            var newElement = document.createElement("p");
+            element.appendChild(newElement);
+            newElement.innerHTML = "Got'm!" + "<br> Damage this round: " + damageThisRound +  "<br> Total damage: " + totalDamage;
+ 
+            if(totalDamage>=4){
+ 
+                var element = document.getElementById("gameStatus");
+                var newElement = document.createElement("p");
+                element.appendChild(newElement);
+                newElement.innerHTML = "<br> He's dead!";
+  
+                slaying=false; 
+            //endif(totalDamage>=4){}else{}
+            };
 
-}
+            //else if youHit = false
+        }else{
 
+            var element = document.getElementById("gameStatus");
+            var newElement = document.createElement("p");
+            element.appendChild(newElement);
+            newElement.innerHTML = "<br> You missed! He killed you. You lose!";   
 
+            slaying=false;  
+            
+         //end if(youHit){}else{}   
+        };
+    //end while(slaying)
+    };
+//end function dragonSlayerFunction()
+};
+//end window.onload = function ()
 };
