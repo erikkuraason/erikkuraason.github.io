d49dbc120a657e0226c5e9c59c040805b1dfed6c
LarsonDataWonk <entrepreneurial.spirit@outlook.com>
2017-01-09 19:01:34 -0500

    Add files via upload

--- rps/rps.js
+++ rps/rps.js
@@ -1,49 +1,47 @@
-//window.onload = function () {
-  function rpsFunction(){
- 
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
-}; 
+window.onload = function () {
+
+$('#one').append('<form>Do you choose rock, paper or scissors?<br> <input type="text" name="message" value=""></form><button id="button">Go!</button>');
+
+    $('button').click(function() {
+
+        var userChoice = $("input[name=message]").val();
+        var computerChoice = Math.random();
+
+        if (computerChoice < 0.34) {
+	        computerChoice = "rock";
+        } else if(computerChoice <= 0.67) {
+	        computerChoice = "paper";
+        } else {
+	        computerChoice = "scissors";
+        }; 
 
 
 
-var compare = function (choice1, choice2){
+    var compare = function (choice1, choice2){
 
-    if (choice1 === choice2) {
-        return "The result is a tie!";
-    }
-    else if(choice1==="rock"){
-        if(choice2 === "scissors"){
+        if (choice1 === choice2) {
+            return "The result is a tie!";
+        }else if(choice1==="rock"){
+            if(choice2 === "scissors"){
             return "Rock wins. You win.";
-        }
-        else {
+            }else {
             return "Paper wins. You lose.";
-        }
-    }
-    else if (choice1 === "paper"){
-        if (choice2 === "rock"){
-            return "Paper wins. You win";
-        }
-        else {
-            return "Scissors wins. You lose."
-        }
-    }
-    else if (choice1 === "scissors"){
-        if (choice2 === "rock"){
-            return "Rock wins. You lose.";
-        }
-        else {
-            return "Scissors wins. You lose";
-        }
+            }
+        }else if (choice1 === "paper"){
+            if (choice2 === "rock"){
+                return "Paper wins. You win";
+            }else {
+                return "Scissors wins. You lose."
+            }
+        }else if (choice1 === "scissors"){
+            if (choice2 === "rock"){
+                return "Rock wins. You lose.";
+            }else {
+                return "Scissors wins. You lose";
+            }
     
-    }
-};
+        }
+    }   ;
 
 
 var element1 = document.getElementById("gameStatus");
@@ -61,10 +59,7 @@ var newElement3 = document.createElement("p");
 element3.appendChild(newElement3);
 newElement3.innerHTML = compare(userChoice, computerChoice);
 
-
-
-
-
+    });
 
 
 };  
